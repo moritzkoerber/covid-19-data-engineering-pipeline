396d7668e2b6efa58c89066c78ef9a0c2628bdf9
moritzkoerber <koerber.moritz@gmail.com>
2021-11-13 17:13:54 +0100

    add stack

--- setup_aws.sh
+++ setup_aws.sh
@@ -3,4 +3,4 @@
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account)
 AWS_REGION=$(aws configure get region)
 
-aws cloudformation deploy --template-file ops/stack.yaml --stack-nam data-engineering-stack
+aws cloudformation deploy --template-file ops/stack.yaml --stack-name data-engineering-stack --capabilities CAPABILITY_IAM
