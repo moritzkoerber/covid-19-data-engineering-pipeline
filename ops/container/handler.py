2b0c363685a8d69f6a5d2b61b1d1133718066dbf
moritzkoerber <koerber.moritz@gmail.com>
2021-12-04 17:48:18 +0100

    Change handler save location

--- ops/container/handler.py
+++ ops/container/handler.py
@@ -12,7 +12,9 @@ def handler(event, context):
     response = requests.get(_API_URL)
     df = pandas.json_normalize(response.json())
 
+    date = f"{pandas.to_datetime(df['meta.lastUpdate'][0]):%Y-%m-%d}"
+
     df.to_parquet(
-        f"{_BUCKET}/vaccinations/{pandas.Timestamp.today():%Y-%m-%d}.parquet",
+        f"{_BUCKET}/vaccinations/date={date}/{date}.parquet",
         index=False,
     )
