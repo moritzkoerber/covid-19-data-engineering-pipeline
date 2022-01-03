70e3d927a689a3aef271fd2d2497dd12ee417545
moritzkoerber <koerber.moritz@gmail.com>
2022-01-03 22:21:28 +0100

    move api lambda docker files

--- ops/container/api_lambda/handler.py
+++ ops/container/api_lambda/handler.py
@@ -4,17 +4,16 @@ import pandas
 import requests
 
 _API_URL = os.environ["API_URL"]
-_BUCKET = os.environ["BUCKET"]
+_S3_BUCKET = os.environ["_S3_BUCKET"]
 
 
 def handler(event, context):
-
     response = requests.get(_API_URL)
     df = pandas.json_normalize(response.json())
 
     date = f"{pandas.to_datetime(df['meta.lastUpdate'][0]):%Y-%m-%d}"
 
     df.to_parquet(
-        f"{_BUCKET}/vaccinations/date={date}/{date}.parquet",
+        f"{_S3_BUCKET}/vaccinations/raw/{date}.parquet",
         index=False,
     )
