440d53d2662156b36b34e2fa8c8780edad135725
moritzkoerber <koerber.moritz@gmail.com>
2022-01-09 19:56:44 +0100

    remove unnecessary setup file

--- setup_aws.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account)
-AWS_REGION=$(aws configure get region)
-
-aws cloudformation deploy --template-file ops/stack.yaml --stack-name data-engineering-stack --capabilities CAPABILITY_IAM
