996eb73390696cbfc7cba5dfa388098ab269310a
moritzkoerber <koerber.moritz@gmail.com>
2022-02-03 18:30:19 +0100

    glue

--- pipeline/glue_jobs/vaccinations_data_processing.py
+++ pipeline/glue_jobs/vaccinations_data_processing.py
@@ -1,15 +1,30 @@
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from pyspark.context import SparkContext
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
+
+sc = SparkContext()
+glueContext = GlueContext(SparkContext.getOrCreate("default"))
+sc = glueContext.spark_session
+logger = glueContext.get_logger()
+job = Job(glueContext)
 
 sc = SparkSession.builder.getOrCreate()
 
 sdf = sc.read.parquet(
-    "s3://data-engineering-project-production/data/vaccinations/valid/"
+    "s3://data-pipeline-s3-bucket-production/data/vaccinations/valid/"
 )
 
 sdf.select(
     [
         col(x).alias(y)
-        for x, y in zip(sdf.columns, [x.replace(".", "_") for x in sdf.columns])
+        for x, y in zip(
+            sdf.columns,
+            [f"`{x}`".replace(".", "_").replace("`", "") for x in sdf.columns],
+        )
     ]
-).write.parquet("s3://data-engineering-project-production/data/vaccinations/processed/")
+).write.parquet("s3://data-pipeline-s3-bucket-production/data/vaccinations/processed/")
+
+
+job.commit()
