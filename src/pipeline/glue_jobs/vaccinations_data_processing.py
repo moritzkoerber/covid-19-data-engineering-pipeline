8207aef250d4f9c508763fa1ae930b61c15ea189
moritzkoerber <koerber.moritz@gmail.com>
2022-02-06 17:54:04 +0100

    Update glue job

--- src/pipeline/glue_jobs/vaccinations_data_processing.py
+++ src/pipeline/glue_jobs/vaccinations_data_processing.py
@@ -1,30 +1,75 @@
-from awsglue.context import GlueContext
-from awsglue.job import Job
-from pyspark.context import SparkContext
+import sys
+from functools import reduce
+
+import boto3
+from awsglue.utils import getResolvedOptions
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 
-sc = SparkContext()
-glueContext = GlueContext(SparkContext.getOrCreate("default"))
-sc = glueContext.spark_session
-logger = glueContext.get_logger()
-job = Job(glueContext)
+args = getResolvedOptions(sys.argv, ["bucket", "prefix", "write_location"])
+_BUCKET = args["bucket"]
+_PREFIX = args["prefix"]
+_WRITE_LOCATION = args["write_location"]
 
 sc = SparkSession.builder.getOrCreate()
 
-sdf = sc.read.parquet(
-    "s3://data-pipeline-s3-bucket-production/data/vaccinations/valid/"
+read_schema = ",".join(
+    [
+        "cases long",
+        "deaths long",
+        "recovered long",
+        "weekIncidence double",
+        "casesPer100k double",
+        "casesPerWeek long",
+        "`delta.cases` long",
+        "`delta.deaths` long",
+        "`delta.recovered` long",
+        "`r.value` double",
+        "`r.rValue4Days.value` double",
+        "`r.rValue4Days.date` string",
+        "`r.rValue7Days.value` double",
+        "`r.rValue7Days.date` string",
+        "`r.lastUpdate` string",
+        "`hospitalization.cases7Days` long",
+        "`hospitalization.incidence7Days` double",
+        "`hospitalization.date` string",
+        "`hospitalization.lastUpdate` string",
+        "`meta.source` string",
+        "`meta.contact` string",
+        "`meta.info` string",
+        "`meta.lastUpdate` string",
+        "`meta.lastCheckedForUpdate` string",
+    ]
+)
+
+s3_client = boto3.client("s3")
+
+initial_sdf = sc.createDataFrame([], schema=read_schema)
+
+sdf = reduce(
+    lambda x, y: x.union(
+        sc.read.parquet(
+            f"s3://{_BUCKET}/{y}",
+            schema=read_schema,
+        )
+    ),
+    [
+        e["Key"]
+        for e in s3_client.list_objects(Bucket=_BUCKET, Prefix=_PREFIX)["Contents"]
+    ],
+    initial_sdf,
 )
 
 sdf.select(
-    [
-        col(x).alias(y)
-        for x, y in zip(
-            sdf.columns,
-            [f"`{i}`".replace(".", "_").replace("`", "") for i in sdf.columns],
-        )
-    ]
-).write.parquet("s3://data-pipeline-s3-bucket-production/data/vaccinations/processed/")
-
-
-job.commit()
+    [col(x).alias(y) for x, y in [(f"`{i}`", i.replace(".", "_")) for i in sdf.columns]]
+).drop(
+    "meta_source",
+    "meta_contact",
+    "meta_info",
+    "meta_lastUpdate",
+    "meta_lastCheckedForUpdate",
+).write.mode(
+    "overwrite"
+).parquet(
+    f"s3://{_BUCKET}/{_WRITE_LOCATION}"
+)
