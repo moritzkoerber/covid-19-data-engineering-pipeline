17d701050fc68c73f6df364cba4d46de9730cdea
moritzkoerber <koerber.moritz@gmail.com>
2022-09-07 22:03:28 +0200

    airflow dag

--- airflow/dags/download_from_s3.py
+++ airflow/dags/download_from_s3.py
@@ -11,12 +11,16 @@ default_args = {"start_date": datetime(2022, 8, 31, 19, 40), "owner": "Airflow"}
 def read_s3(bucket_name):
     boto3_session = boto3.Session(profile_name="GenericUser")
     s3_client = boto3_session.client("s3")
-    print(s3_client.get_bucket_versioning(Bucket=bucket_name))
+    s3_client.download_file(
+        "mytestbucketlol2",
+        "date=2021-12-10/2021-12-10.parquet",
+        "/opt/airflow/scripts/2021-12-10.parquet",
+    )
 
 
 with DAG(
     dag_id="s3",
-    schedule_interval="*/5 * * * *",
+    schedule_interval="0 0 * * *",
     catchup=False,
     default_args=default_args,
 ) as dag:
