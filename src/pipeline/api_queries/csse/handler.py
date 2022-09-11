8c00c9be1310fa8faa72b301a7957daee6ed8869
moritzkoerber <koerber.moritz@gmail.com>
2022-09-11 17:52:09 +0200

    rename to csse

--- src/pipeline/api_queries/csse/handler.py
+++ src/pipeline/api_queries/csse/handler.py
@@ -1,6 +1,7 @@
 import base64
 import io
 import os
+import re
 
 import awswrangler as awr
 import pandas as pd
@@ -12,12 +13,18 @@ _ENVIRONMENT = os.environ["ENVIRONMENT"]
 
 _now = pd.Timestamp.utcnow()
 # countries = [e.name for e in list(pycountry.countries)]
-file_names = ["time_series_covid19_deaths_global.csv"]
-table_names = ["deaths_global"]
+file_names = [
+    "time_series_covid19_confirmed_global.csv",
+    "time_series_covid19_deaths_global.csv",
+    "time_series_covid19_recovered_global.csv",
+]
+
+regex_pattern = re.compile(r"(?:covid19_)(\w+)(?:_global)")
+data_types = [regex_pattern.search(file_name)[1] for file_name in file_names]
 
 
 def handler(event, context):
-    for file_name, table_name in zip(file_names, table_names):
+    for file_name, data_type in zip(file_names, data_types):
         file = requests.get(f"{_REPOSITORY_PATH}/{file_name}")
         file_obj = io.StringIO(base64.b64decode(file.json()["content"]).decode("ascii"))
 
@@ -36,10 +43,10 @@ def handler(event, context):
 
         awr.s3.to_parquet(
             pandas_df,
-            f"{_S3_BUCKET}/global/{table_name}/",
+            f"{_S3_BUCKET}/data/{data_type}/global/",
             schema_evolution=False,
             database=f"api_data_{_ENVIRONMENT}",
-            table=table_name,
+            table=f"{data_type}_global",
             mode="overwrite_partitions",
             sanitize_columns=True,
             partition_cols=["year", "iso_week"],
