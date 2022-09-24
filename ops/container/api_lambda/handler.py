28d310b8c42d46cf0cf9766eed5d7c455aceded8
Moritz Körber <koerber.moritz@gmail.com>
2022-09-24 18:33:07 +0200

    Add vaccinations datasource (#17)

--- ops/container/api_lambda/handler.py
+++ ops/container/api_lambda/handler.py
@@ -8,16 +8,32 @@ logger = logging.getLogger()
 
 _API_URL = os.environ["API_URL"]
 _S3_BUCKET = os.environ["S3_BUCKET"]
+_NOW = pd.Timestamp.utcnow()
 
 
 def handler(event, context):
-    # german data
+    # cases
+    logger.info("Querying cases...")
     response = requests.get(f"{_API_URL}/germany")
     df = pd.json_normalize(response.json())
 
     date = f"{pd.to_datetime(df['meta.lastUpdate'][0]):%Y-%m-%d}"
 
-    df.assign(api_call_ts_utc=pd.Timestamp.utcnow()).to_parquet(
+    df.assign(api_call_ts_utc=_NOW).to_parquet(
         f"{_S3_BUCKET}/data/rki/raw/germany/cases/{date}.parquet",
         index=False,
     )
+    logger.info("Cases successfully queried.")
+
+    # vaccinations
+    logger.info("Querying vaccinations...")
+    response = requests.get(f"{_API_URL}/vaccinations")
+    df = pd.json_normalize(response.json())
+
+    date = f"{pd.to_datetime(df['meta.lastUpdate'][0]):%Y-%m-%d}"
+
+    df.assign(api_call_ts_utc=_NOW).to_parquet(
+        f"{_S3_BUCKET}/data/rki/raw/germany/vaccinations/{date}.parquet",
+        index=False,
+    )
+    logger.info("Vaccinations successfully queried.")
