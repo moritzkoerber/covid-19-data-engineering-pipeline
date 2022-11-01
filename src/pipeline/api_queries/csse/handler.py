a8699a312c260d398872020252e7abe3b659c16a
moritzkoerber <koerber.moritz@gmail.com>
2022-11-01 22:37:01 +0100

    Update github URLs

--- src/pipeline/api_queries/csse/handler.py
+++ src/pipeline/api_queries/csse/handler.py
@@ -1,4 +1,3 @@
-import base64
 import io
 import os
 import re
@@ -26,7 +25,7 @@ data_types = [regex_pattern.search(file_name)[1] for file_name in file_names]
 def handler(event, context):
     for file_name, data_type in zip(file_names, data_types):
         file = requests.get(f"{_REPOSITORY_PATH}/{file_name}")
-        file_obj = io.StringIO(base64.b64decode(file.json()["content"]).decode("ascii"))
+        file_obj = io.StringIO(file.text)
 
         pandas_df = (
             pd.read_csv(file_obj)
